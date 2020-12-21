76d741a33c81f91879e0f1bbdde32b2f23669bad
John Leigh <44871530+johnleigh517@users.noreply.github.com>
2020-12-21 17:36:39 +0000

    Update SVC_Classifier.py
    Added Cross Validation

--- SVC_Classifier.py
+++ SVC_Classifier.py
@@ -13,11 +13,15 @@ Each function has a docsting about its function
 """
 
 import json
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score,recall_score
 import pandas as pd
+from sklearn.model_selection import KFold
+from matplotlib.gridspec import GridSpec
+import matplotlib.pyplot as plt
 
 
 def get_dataset():
@@ -79,14 +83,85 @@ def svm(X, y):
     print('F1 Score: ', f1_score(y_test, predict, labels=labels, average='micro'))
     print('Precision Score: ', precision_score(y_test, predict, labels=labels, average='micro'))
     print('Accuracy Score: ', accuracy_score(y_test, predict))
+    print('Recall: ',recall_score(y_test, predict, labels=labels,average='macro'))
+    
+def cross_val(k,model,X,y):
+    """
+    Inputs: kfold number, machine learning model, data to evaluate X,y
+    
+    Outputs: The accuracy, recall and precision and their respective standard deviations for the model
+    """
+    accuracy_list=[]
+    recall_list=[]
+    precision_list=[]
+    labels = ['Positive', 'Negative', 'Neutral']
+    kf=KFold(n_splits=k)
+    for train, test in kf.split(X):
+    
+        model.fit(X[train],y[train])
+        predict=model.predict(X[test])
+        
+        accuracy=accuracy_score(y[test], predict)
+        recall=recall_score(y[test], predict, labels=labels,average='macro')
+        precision=precision_score(y[test], predict, labels=labels,average='macro')
+        accuracy_list.append(accuracy)
+        recall_list.append(recall)
+        precision_list.append(precision)
+        
+    accuracy_end=np.mean(accuracy_list)
+    std=np.std(accuracy_list)
+    recall=(np.mean(recall_list),np.std(recall_list))
+    precision=(np.mean(precision_list),np.std(precision_list))
+    
+    print('Accuracy: ', accuracy_end)
+    print('Standard Deviation', std)
+    print('Recall', recall)
+    print('Precision', precision)
+    
+    return accuracy_end, std ,recall, precision
 
+def plot_cross_val(X,y):
+    """
+    Purpose:
+        Plots the accuracy and standard deviation for different gamma, C
+        Using to fine tune these parameters
+    """
+
+    gamma=[0.1,1,10,100]
+    C=[0.1,1,10,100]
+    plotx=[0,1,0,1] #lists for plotting
+    ploty=[0,0,1,1] #lists for plotting
+    gs = GridSpec(2, 2, wspace=0.3, hspace=0.3)
+    fig=plt.figure()
+    plt.rc('font', size=18)
+    l=0
+    for i in gamma:
+
+        gx=plotx[l]
+        gy=ploty[l]
+        ax = fig.add_subplot(gs[gx, gy])
+        accuracy_list=[]
+        std_list=[]
+        for c in C:
+            
+            model=SVC(kernel='rbf', gamma=i, C=c)
+            accuracy,std=cross_val(5,model,X,y)
+            accuracy_list.append(accuracy)
+            std_list.append(std)
+        plt.errorbar(C,accuracy_list,yerr=std_list)
+        ax.set_title('Gamma = '+str(i))
+        ax.set_ylabel('Accuracy')
+        ax.set_xlabel('C')
+        plt.xscale('log')
+        l=l+1
 
 def main():
     data = get_dataset()
     X, y = tf_idf(data)
     # hyper_pick(X, y)
     svm(X, y)
-
+    cross_val(5,SVC(kernel='rbf', gamma=1, C=100),X,y)
+    #plot_cross_val(X,y)
     return
 
 
